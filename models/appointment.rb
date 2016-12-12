51230c0fabd3422b0001cb11b797de67c7d66936
Young Ju Park <parkyngj@gmail.com>
2016-12-12 14:45:52 -0800

    Initialize controller and runner file

--- models/appointment.rb
+++ models/appointment.rb
@@ -2,11 +2,12 @@ class Appointment
   def initialize(args)
     @client_name = args.fetch(:client_name)
     @client_phone = args.fetch(:client_phone)
+    set_date_times(args)
   end
 
   private
 
   def set_date_times(args)
-
+    
   end
 end
