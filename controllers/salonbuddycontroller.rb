8bd8ddcdf2d55c72f65d8b6e2accd81154e27ada
Young Ju Park <parkyngj@gmail.com>
2016-12-12 15:47:51 -0800

    add cases to user input from menu in controller

--- controllers/salonbuddycontroller.rb
+++ controllers/salonbuddycontroller.rb
@@ -6,7 +6,18 @@ class SalonBuddyController
     end
 
     def menu_handler
-      user_input = MenuViewer.menu
+      user_input = MenuViewer.menu.downcase.split(" ")
+
+      case user_input.first
+        when "list"
+          "I totally didn't implement this yet."
+        when "schedule"
+          "I totally didn't implement this yet."
+        else
+          puts "Invalid input. Please try again."
+          user_input = Viewer.menu.downcase
+        end
+      end
     end
 
     def run
