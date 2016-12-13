6f37e7b57b539fc2499ac4a1bc959071f710eb78
Young Ju Park <parkyngj@gmail.com>
2016-12-13 15:08:15 -0800

    add puts statement for success upon creation of appointment

--- views/schedule_viewer.rb
+++ views/schedule_viewer.rb
@@ -36,7 +36,7 @@ module ScheduleViewer
   end
 
   def self.success(appointment)
-    "Booked your #{appointment.class} apppointment from #{appointment.start_datetime.strftime('%I:%M%p')} to #{appointment.end_datetime.strftime('%I:%M%p')} with #{appointment.client_name}."
+    puts "Booked your #{appointment.class} apppointment from #{appointment.start_datetime.strftime('%I:%M%p')} to #{appointment.end_datetime.strftime('%I:%M%p')} with #{appointment.client_name}."
   end
 
   def self.invalid_input(field)
