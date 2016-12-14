7be4aa11ce67a3a900f27fee3d2945424067d39c
Young Ju Park <parkyngj@gmail.com>
2016-12-14 07:36:40 -0800

    write all methods for validations, formatting inputs, formatting outputs in schedule helper

---
+++ helpers/schedule_helper.rb
@@ -0,0 +1,37 @@
+module ScheduleHelper
+  def self.format_name_input
+
+  end
+
+  def self.format_phone_input
+
+  end
+
+  def self.validate_phone_format
+
+  end
+
+  def self.format_phone
+
+  end
+
+  def self.validate_date_format
+
+  end
+
+  def self.validate_date_past
+
+  end
+
+  def self.format_time_input
+
+  end
+
+  def self.validate_time_format
+
+  end
+
+  def self.validate_time_past
+
+  end
+end
