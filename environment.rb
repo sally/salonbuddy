622824aa205e4575fa9b3194b7e083c5ab5ff642
Young Ju Park <parkyngj@gmail.com>
2016-12-14 07:14:08 -0800

    require models, helpers, views in environment

--- environment.rb
+++ environment.rb
@@ -1,4 +1,5 @@
 project_root = File.dirname(File.absolute_path(__FILE__))
 Dir.glob(project_root + '/controllers/*') {|file| require file}
-
-# Dir[File.dirname(__FILE__) + '/controllers/*.rb'].each {|file| p file }
+Dir.glob(project_root + '/helpers/*') {|file| require file}
+Dir.glob(project_root + '/models/*') {|file| require file}
+Dir.glob(project_root + '/views/*') {|file| require file}
