1edface0f552a89c95aaaafad7987486c9b9d045
Young Ju Park <parkyngj@gmail.com>
2016-12-14 16:49:44 -0800

    remove comment about pending method for adding appointment and sorting in schedule

--- models/schedule.rb
+++ models/schedule.rb
@@ -5,7 +5,6 @@ class Schedule
     @appointments = []
   end
 
-  # temporary placeholder for method that will auto-sort appointments by date upon insertion of appt obj
   def add_appointment(appointment)
     appointments.push(appointment).sort! {|appt1, appt2| appt1.start_datetime <=> appt2.start_datetime}
   end
